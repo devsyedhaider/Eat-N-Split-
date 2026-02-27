e0bd5df87d646081a02620918d1b5c8a1c623d4c
devsyedhaider <devalihaider86@gmail.com>
2026-02-27 14:26:04 +0500

    add friend list

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,54 @@ const initialFriends = [
 console.log(initialFriends);
 
 function App() {
-  return <></>;
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendList />
+      </div>
+    </div>
+  );
+}
+
+// Freind List parent
+
+function FriendList() {
+  const Freinds = initialFriends;
+
+  return (
+    <ul>
+      {Freinds.map((friend) => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
+}
+
+// Friend list
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} {Math.abs(friend.balance)}€
+        </p>
+      )}
+
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you {Math.abs(friend.balance)}€
+        </p>
+      )}
+
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+
+      <button className="button">Select</button>
+    </li>
+  );
 }
 
 export default App;
